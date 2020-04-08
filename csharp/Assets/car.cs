fb95204fd19e064c7cfb3f047611e83953ea1a77
zotzu2000 <63327702+zotzu2000@users.noreply.github.com>
2020-04-08 15:29:32 +0800

    認識欄位

--- csharp/Assets/car.cs
+++ csharp/Assets/car.cs
@@ -3,26 +3,59 @@ using UnityEngine;
 
 // 註解 : 用來說明紀錄等等.. 只會在此腳本內看到
 // 摘要 summary : 描述此內容，會在其他腳本內看到 - 提高維護性與擴充性
+// 新增的 c# 放在專案內為藍圖
+// 放在場景物件上為實體物件
 /// <summary>
-/// 
+/// 定義汽車物件
 /// </summary>
 public class car : MonoBehaviour
 {
     // 認識第一個成員 : 欄位 field * 儲存物件資料
+
     // 資料類型
-    // C C 數  - 2000       - 整數 int
+    // C C 數  - 2000     - 整數 int
     // 重量  - 100.9      - 浮點數 float    
     // 品牌  - BMW        - 字串 string
     // 剎車  -是否開啟    - 布林值 bool  - true 、 false
 
     // 定義欄位語法 :
     // 資料類型 欄位名稱 (指定 值);
+
+    // 修飾詞 :
+    // 公開 public : 允許其他類別存取、顯示在屬性面板(Inspector)上
+    // 私人 private (預設) : 不允許、不顯示
+
     // 預設值 
     // int 、float為 0
     // string 為 ""
     // bool 為 false
-    int cc = 2000;
-    float weight = 100.9f;
-    string brand = "BMW";
-    bool brake = false;
+    // 欄位屬性 Attribute : Unity 提供用於輔助欄位的功能
+    // 語法 : [屬性名稱(值)]
+    // 標題 [Heater("字串")]
+    // 提示 [Tooltip("字串")]
+    // 範圍 [Range(最小值，最大值)] - 僅限於數值類型資料 int、float
+
+    /// <summary>
+    /// 汽車的 CC 數
+    /// </summary>
+    [Header("汽車的 CC 數"), Range(500, 5000)]
+    public int cc = 2000;
+    /// <summary>
+    /// 汽車的重量
+    /// </summary>
+    [Header("汽車的重量")]
+    [Tooltip("這個欄位是用來存放汽車的噸位")]
+    [Range(500, 5000)]
+    public float weight = 100.9f;
+    /// <summary>
+    /// 品牌
+    /// </summary>
+    [Header("品牌")]
+    [Tooltip("代表車子的品牌")]
+    public string brand = "BMW";
+    /// <summary>
+    /// 是否剎車
+    /// </summary>
+    [Header("是否剎車"), Tooltip("用來儲存這台車子目前是否為剎車狀態")]
+    public bool brake = false;
 }
--- csharp/Assets/class1.cs
+++ csharp/Assets/class1.cs
@@ -11,6 +11,12 @@ public class class1 : MonoBehaviour
     // 1.大小不一樣
     // 2. 括號成對出現 () [] {} '' "" <>
 
+    [Header("汽車 1 號")]
+    public car car1;
+
+    [Header("汽車 2 號")]
+    public car car2;
+
     // 事件 : 在特定時間會已指定次數執行的方法 (名稱為藍色)
     // 開始事件 : 撥放遊戲時執行一次 - 初始事件
     private void Start()
@@ -18,6 +24,16 @@ public class class1 : MonoBehaviour
         // 輸出("文字訊息");
         // c# 結尾為分號 ;
         print("Hello World~");
+
+        // 取得 get
+        print("汽車 1 號的 CC 數 : " + car1.cc);
+        print("汽車 2 號的品牌 : " + car2.brand);
+
+        // 設定 set
+        car1.cc = 9999;
+        car1.weight = 999.9f;
+        car1.brand = "TOYOTA";
+        car1.brake = true;
     }
   
 }

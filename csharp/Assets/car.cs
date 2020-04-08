f58359790e1103efbbb7cefe48b76e13a9b0cd89
zotzu2000 <63327702+zotzu2000@users.noreply.github.com>
2020-04-08 11:54:02 +0800

    新增 C#

---
+++ csharp/Assets/car.cs
@@ -0,0 +1,28 @@
+﻿
+using UnityEngine;
+
+// 註解 : 用來說明紀錄等等.. 只會在此腳本內看到
+// 摘要 summary : 描述此內容，會在其他腳本內看到 - 提高維護性與擴充性
+/// <summary>
+/// 
+/// </summary>
+public class car : MonoBehaviour
+{
+    // 認識第一個成員 : 欄位 field * 儲存物件資料
+    // 資料類型
+    // C C 數  - 2000       - 整數 int
+    // 重量  - 100.9      - 浮點數 float    
+    // 品牌  - BMW        - 字串 string
+    // 剎車  -是否開啟    - 布林值 bool  - true 、 false
+
+    // 定義欄位語法 :
+    // 資料類型 欄位名稱 (指定 值);
+    // 預設值 
+    // int 、float為 0
+    // string 為 ""
+    // bool 為 false
+    int cc = 2000;
+    float weight = 100.9f;
+    string brand = "BMW";
+    bool brake = false;
+}
---
+++ csharp/Assets/class1.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;          // 引用Unity API(函式庫 : Unity 屬性、功能...)
+
+// 類別 class 定義一個物件
+// 語法: class 類別名稱
+// : MonoBehavior 可以將此腳本掛在 Unity 物件上
+// 類別必須放在物件上才會執行
+public class class1 : MonoBehaviour
+{
+    // 定義類別成員
+    // c#
+    // 1.大小不一樣
+    // 2. 括號成對出現 () [] {} '' "" <>
+
+    // 事件 : 在特定時間會已指定次數執行的方法 (名稱為藍色)
+    // 開始事件 : 撥放遊戲時執行一次 - 初始事件
+    private void Start()
+    {
+        // 輸出("文字訊息");
+        // c# 結尾為分號 ;
+        print("Hello World~");
+    }
+  
+}

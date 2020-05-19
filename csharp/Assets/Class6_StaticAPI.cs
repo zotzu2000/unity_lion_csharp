facfb5b1904eb1561a563c3d55e67755125520ea
zotzu2000 <63327702+zotzu2000@users.noreply.github.com>
2020-05-19 14:02:22 +0800

    認識如何取得非靜態欄位

--- csharp/Assets/Class6_StaticAPI.cs
+++ csharp/Assets/Class6_StaticAPI.cs
@@ -5,6 +5,14 @@ public class Class6_StaticAPI : MonoBehaviour
     //物件
     public Enemy zombie1, zombie2;
 
+    //修飾詞 非靜態成員類別 物件名稱
+    public GameObject obj1;
+
+    public Light myLight;
+
+    public Transform tran1;
+    public Transform tran2;
+
     private void Start()
     {
         #region 靜態練習
@@ -33,6 +41,7 @@ public class Class6_StaticAPI : MonoBehaviour
         //zombie1.Stop();
         #endregion
 
+        #region 靜態成員
         //API 靜態成員
 
         //存放 靜態 屬性
@@ -53,6 +62,27 @@ public class Class6_StaticAPI : MonoBehaviour
 
         //整數不傳回最大值
         print("隨機整數 : " + Random.Range(1, 3));
+        #endregion
+
+        //錯誤 : 非靜態不能透過類別存取
+        //print(GameObject.layer);
+
+        //靜態屬性與非靜態屬性區別
+        //靜態屬性 不需要新增欄位  -   Random.value
+        //非靜態屬性 需要新增欄位  -   public GameObject obj1;   obj1.layer
+
+        //非靜態 屬性
+        //物件名稱.非靜態屬性
+        print("物件圖層 : " + obj1.layer);
+
+        //非靜態 需要有實體物件   -   存在遊戲場景內的物件
+        print("燈光顏色 : " + myLight.color);
+
+        //tran1.localScale = new Vector3(5, 5, 5);
+
+        //Vector3.one = new Vector3(1, 1, 1);
+        tran1.localScale = Vector3.one * 5;
+        tran2.localScale = Vector3.one * 3;
     }
 
     //偵測玩家輸入 - 約 60 FPS
@@ -62,5 +92,10 @@ public class Class6_StaticAPI : MonoBehaviour
         print("玩家是否按空白鍵 : " + Input.GetKeyDown("space"));
         //多載 1 : 列舉
         print("玩家是否按右鍵 : " + Input.GetKeyDown(KeyCode.Mouse1));
+
+        //非靜態方法
+        //物件名稱.非靜態方法(對應的引數)
+        tran1.Rotate(0, 0, 10);
+        tran2.Rotate(0, 0, -10);
     }
 }

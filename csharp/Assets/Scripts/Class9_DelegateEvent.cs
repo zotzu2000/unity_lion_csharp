0528cef363ddf4e0167dce95543a54553c9d5f34
zotzu2000 <63327702+zotzu2000@users.noreply.github.com>
2020-06-23 16:02:57 +0800

    第八章練習

--- csharp/Assets/Scripts/Class9_DelegateEvent.cs
+++ csharp/Assets/Scripts/Class9_DelegateEvent.cs
@@ -31,4 +31,32 @@ public class Class9_DelegateEvent : MonoBehaviour
     {
         print("我是方法 C - " + msg);
     }
+
+    // 定義委派
+    // 可以儲存 無傳回、無參數 的任何方法
+    public delegate void DelegateTest();
+
+    // 可以儲存 無傳回、一個字串參數 的任何方法
+    public delegate void DelegateTest2(string s);
+    // 定義委派欄位
+    public DelegateTest dA;
+    public DelegateTest dB;
+    public DelegateTest dC;
+
+    public DelegateTest2 d2C;
+
+    private void Start()
+    {
+        dA = MethodA;   // dA 欄位儲存 方法 A
+        dA();           // 呼叫 dA
+
+        dB = MethodB;
+        dB();
+
+        // dA = MethodC;	// 不同簽名無法儲存
+        // dC();
+
+        d2C = MethodC;      // 儲存 方法 C
+        d2C("我是委派");
+    }
 }
